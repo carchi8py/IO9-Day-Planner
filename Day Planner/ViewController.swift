a5b52fbff4b2277888311b1fa37d402cc1d57f0e
Chris Archibald <carchi8py@gmail.com>
2016-01-09 20:10:40 -0800

    Initial Commit

---
+++ Day Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day Planner
+//
+//  Created by Chris Archibald on 1/9/16.
+//  Copyright © 2016 Chris Archibald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
