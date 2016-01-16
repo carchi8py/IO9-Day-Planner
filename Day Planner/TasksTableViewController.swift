191e03a3778917e7e5b7b82a1705ec3e311a63e7
Chris Archibald <carchi8py@gmail.com>
2016-01-15 22:16:20 -0800

    Added our edit table methods.

--- Day Planner/TasksTableViewController.swift
+++ Day Planner/TasksTableViewController.swift
@@ -138,25 +138,27 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
         cell.detailTextLabel?.text = taskStatus
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let context = self.fetchedResultsController.managedObjectContext
+            context.deleteObject(self.fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject)
+            do {
+                try context.save()
+            } catch _ {
+                let alertController = UIAlertController(title: "Error", message: "Error in deleting the Record", preferredStyle: .Alert)
+                alertController.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+        }
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
