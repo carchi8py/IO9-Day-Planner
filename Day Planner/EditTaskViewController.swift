7727d23e74b273aa148ea42d404f8debc474b6f2
Chris Archibald <carchi8py@gmail.com>
2016-01-15 22:32:43 -0800

    Wiring up part of the Task View Controller.

--- Day Planner/EditTaskViewController.swift
+++ Day Planner/EditTaskViewController.swift
@@ -7,16 +7,47 @@
 //
 
 import UIKit
+import CoreData
 
-class EditTaskViewController: UIViewController {
+class EditTaskViewController: UIViewController, UITextFieldDelegate {
 
     var newTask: Bool = true
     var taskItem: AnyObject? = nil
     
+    var appDel: AppDelegate = AppDelegate()
+    var context = NSManagedObjectContext(concurrencyType: NSManagedObjectContextConcurrencyType.MainQueueConcurrencyType)
+    
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var taskStatusLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var statusSwitch: UISwitch!
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        appDel = UIApplication.sharedApplication().delegate as! AppDelegate
+        context = appDel.managedObjectContext
+        
+        taskTextField.delegate = self
+        
+        if newTask == true {
+            saveButton.setTitle("Save", forState: UIControlState.Normal)
+        } else {
+            saveButton.setTitle("Update", forState: UIControlState.Normal)
+        }
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +55,21 @@ class EditTaskViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backPressed(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
 
+    @IBAction func statusSwitchChanged(sender: UISwitch) {
+        if sender.on {
+            taskStatusLabel.text = "Complete"
+        } else {
+            taskStatusLabel.text = "Open"
+        }
+    }
+    
+    @IBAction func saveButtonPressed(sender: AnyObject) {
+    }
+    
     /*
     // MARK: - Navigation
 
